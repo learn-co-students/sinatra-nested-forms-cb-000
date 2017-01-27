c88deb28e6cc00c552b04e5151791f2f021048e8
Alexander Thibodeaux <alyxanderthibodeaux@gmail.com>
2017-01-27 17:01:13 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  PIRATES = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @weight = attributes[:weight]
+    @height = attributes[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end

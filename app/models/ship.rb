e93020945b9d9e5065a62f22dea4e1f5b6c81d0e
github username <github email address>
2017-03-13 10:09:33 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,19 +1,21 @@
 class Ship
   attr_reader :name, :type, :booty
-  SHIPS = []
 
-  def initialize(attributes)
-    @name = attributes[:name]
-    @type = attributes[:type]
-    @booty = attributes[:booty]
-    SHIPS << self
-  end
+  SHIPS = []
 
   def self.all
     SHIPS
   end
 
   def self.clear
-    SHIPS.clear
+  	SHIPS.clear
+  end
+
+  def initialize(ship_info)
+    @name = ship_info[:name]
+    @type = ship_info[:type]
+    @booty = ship_info[:booty]
+
+    SHIPS << self
   end
 end

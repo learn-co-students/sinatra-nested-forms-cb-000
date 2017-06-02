1433a38fb6e2240b51b3e52b08e2b4614ec22b4e
youssefmys <mosa500@gmail.com>
2017-06-02 07:56:41 +0200

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,30 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :"pirates/new"  
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new.tap do |p|
+        p.name = params[:pirate][:name]
+        p.height = params[:pirate][:height]
+        p.weight = params[:pirate][:weight]
+      end
+
+      params[:pirate][:ships].each do |ship_attr|
+        Ship.new(ship_attr)
+      end
+
+      @ships = Ship.all 
+      erb :"pirates/show"
+
+    end
+    
   end
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@ships = [] 
+  
+  def initialize(attributes)
+    attributes.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+  
 end

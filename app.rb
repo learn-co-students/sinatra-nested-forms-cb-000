6d5227883022068e829bca5b9940f4b340ba126f
Christina Anglin <anglin.christina@gmail.com>
2018-01-10 05:11:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,28 @@
+# set :views, Proc.new { File.join(root, "pirates") }
+
 require './environment'
+
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+
+    get '/' do
+      erb :new
+    end
+
+    get '/new' do
+      erb :show
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = params[:pirate][:ships]
+      @ship1 = Ship.new(@ships[0])
+      @ship2 = Ship.new(@ships[1])
+      erb :pirates
+    end
 
   end
 end

f1b530254b03a140b0428c716f1fe9b9d5a326df
Heather Christman <efl7a@yahoo.de>
2018-01-12 15:16:03 -0500

    Lesson Complete

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ships =[]
+
+  def initialize(ship_hash)
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
 end

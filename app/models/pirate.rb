43951cd7a40067ecdebee176f0317bb65a40c4bb
petitlapin86 <skinnerpa86@yahoo.com>
2018-02-19 10:20:05 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+   PIRATES = [] #empty array of pirates
+
+  def initalize(params) #Pirates should have a name, weight, and height.
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self #store pirates in array
+  end
+
+def self.all #You will also need a class method that returns all the pirates
+  PIRATES
+end
+
 end

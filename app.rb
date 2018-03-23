e25deeb0e3594e7a86ecac498fb38c34c2fbbc15
adam n <adam.nawroc@gmail.com>
2018-03-23 11:24:10 +0000

    progress

--- app.rb
+++ app.rb
@@ -5,5 +5,27 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+
+      @pirate = Pirate.new(params[:pirate])
+
+      # p params[:pirate][:ships]
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      # p @ships
+      erb :pirates
+    end
+
   end
 end

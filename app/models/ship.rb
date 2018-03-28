fe40bc2a62052eee903b41ef7928b2cd4a89965d
stritch.dev <stritch.dev@gmail.com>
2018-03-28 13:43:17 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+
+  Ships = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    Ships << self
+  end
+
+  def self.all
+    Ships
+  end
+
+  def self.clear
+    Ships.each { |ship| ship = nil }
+  end
+
 end

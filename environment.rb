51d5bb440221d29e24f1d1744f00c0b8df591dfe
pi-track <patrick.eells@gmail.com>
2018-04-13 11:46:25 -0400

    Done.

--- environment.rb
+++ environment.rb
@@ -3,7 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+require 'sinatra'
 require 'sinatra/base'
-require 'sinatra/reloader'
+#require 'sinatra/reloader'
 
-require_all './app'
+require './app/models/pirate'
+require './app/models/ship'

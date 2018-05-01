c0443dddb15c831335b1d82b5593c8210ccb3b28
Alex Segers <a.segers.dev@gmail.com>
2018-05-01 17:09:23 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,14 +3,18 @@ class Ship
 
   SHIPS = []
 
-  def initialize(name:, type:, booty:)
-    @name = name
-    @type = type
-    @booty = booty
+  def initialize(details)
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
     SHIPS << self
   end
 
   def self.all
     SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
   end
 end

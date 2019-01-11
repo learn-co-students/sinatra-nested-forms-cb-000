cb7e6e533f40e47e66b7b3acf369dfdfd8113137
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-11 20:35:19 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(stuff)
+    @name = stuff["name"]
+    @type = stuff["type"]
+    @booty = stuff["booty"]
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def self.clear
+    @@all = []
+  end
+
 end
